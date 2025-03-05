32cd205e479e4ef25622d3703f69d5474fd0ca48
Anderson Andrade <anderson_andrade_@outlook.com>
2025-03-05 08:02:39 -0300

    refactor(map): implement default images to show wall and walkable space
    is necessary change to better images on the future

--- map.py
+++ map.py
@@ -1,7 +1,7 @@
 import pygame
 from settings import *
 
-map = [
+basic_map = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,1,1,2,2,2,2,2,2,2,2,2,2,2,1,1,0],
     [0,1,0,2,0,2,0,2,0,2,0,2,0,2,0,1,0],
@@ -17,12 +17,18 @@ map = [
 
 class Map:
     def __init__(self):
-        self.map = map
-
-        self.colors = {0: (200, 200, 200), 1: (50, 100, 50), 2: (0, 0, 255)}
+        self.map = basic_map
         self.title_size = 50
+
+        self.textures = {
+            0: pygame.transform.scale(pygame.image.load('assets/textures/not-destructive-wall.png'), (self.title_size, self.title_size)),
+            2: pygame.transform.scale(pygame.image.load('assets/textures/destructive-wall.png'), (self.title_size, self.title_size)),
+        }
 
     def draw(self, screen):
         for position_y, row in enumerate(self.map):
             for position_x, col in enumerate(row):
-                pygame.draw.rect(screen, self.colors[col], (position_x * self.title_size, position_y * self.title_size, self.title_size, self.title_size))
+               if col in self.textures:
+                   screen.blit(self.textures[col], (position_x * self.title_size, position_y * self.title_size))
+               else:
+                   pygame.draw.rect(screen, (50, 100, 50), (position_x * self.title_size, position_y * self.title_size, self.title_size, self.title_size))
