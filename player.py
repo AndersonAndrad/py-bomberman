17a7f98b0531bbfb200b26d12239509659414010
Anderson Andrade <anderson_andrade_@outlook.com>
2025-03-07 19:56:43 -0300

    refactor(player): change variable names to undestand code

--- player.py
+++ player.py
@@ -4,16 +4,16 @@ class Player:
     def __init__(self, game_map):
         self.map = game_map
         self.tile_size = self.map.title_size
-        self.x = 1
-        self.y = 1
+        self.position_x = 1
+        self.position_y = 1
 
-    def move(self, dx, dy):
-        new_x, new_y = self.x + dx, self.y + dy
+    def move(self, direction_x, direction_y):
+        new_position_x= self.position_x + direction_x
+        new_position_y = self.position_y + direction_y
 
-        # Check if the new position is walkable (only walk on '1')
-        if self.map.map[new_y][new_x] == 1:
-            self.x = new_x
-            self.y = new_y
+        if self.map.map[new_position_y][new_position_x] == 1:
+            self.position_x = new_position_x
+            self.position_y = new_position_y
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 0), (self.x * self.tile_size, self.y * self.tile_size, self.tile_size, self.tile_size))
+        pygame.draw.rect(screen, (255, 0, 0), (self.position_x * self.tile_size, self.position_y * self.tile_size, self.tile_size, self.tile_size))
